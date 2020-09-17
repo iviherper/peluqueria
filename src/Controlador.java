545736c02d9b47cd76184e0049f1092f14524a92
DC_Test <DC_Test@DESKTOP-27FDAFT>
2020-09-17 13:15:43 +0200

    Finish

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,8 +1,10 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class Controlador {
 	private Vista miVista;
 	private Modelo miModelo;
-	
+
 	public void setMiModelo(Modelo miModelo) {
 		this.miModelo = miModelo;
 	}
@@ -10,7 +12,7 @@ public class Controlador {
 	public void setMiVista(Vista miVista) {
 		this.miVista = miVista;
 	}
-	
+
 	public void filtrarCli(String[] datos) {
 		String where = "";
 		if (!datos[0].equals(""))
@@ -22,18 +24,25 @@ public class Controlador {
 		if (!datos[3].equals(""))
 			where += " lower(mechas) LIKE lower('" + datos[3] + "') || '%' AND";
 		if (!datos[4].equals("") && !datos[5].equals(""))
-			where += " Fecha between '"+datos[4]+"' and '"+datos[5]+"' AND";
+			where += " Fecha between '" + datos[4] + "' and '" + datos[5] + "' AND";
+		else if (!datos[4].equals("")) {
+			Date myDate = new Date();
+			where += " Fecha between '" + datos[4] + "' and '" + new SimpleDateFormat("dd-MM-yyyy").format(myDate)
+					+ "' AND";
+		} else if (!datos[5].equals("")) {
+			where += " Fecha between '01011800' and '" + datos[5] + "' AND";
+		}
 
 		miModelo.filtrarLisCli(where);
 	}
-	
+
 	public void borrarCli() {
 		String nombre = miVista.getNombre();
 		String apellidos = miVista.getApellidos();
 		String fecha = miVista.getFecha();
 		miModelo.borrarCliente(nombre, apellidos, fecha);
 	}
-	
+
 	public void modificarCliente(String nomAnt, String apeAnt, String fecAnt) {
 		String fecha = miVista.getFecha();
 		String nombre = miVista.getNombre();
@@ -43,14 +52,14 @@ public class Controlador {
 		System.out.println(fecha);
 		miModelo.modificarCliente(nombre, apellidos, tinte, mechas, fecha, nomAnt, apeAnt, fecAnt);
 	}
-	
+
 	public String modificarFecha(String fec) {
 		String Fec_naci = fec.substring(0, 10);
 		String[] fechaa = Fec_naci.split("-");
 		Fec_naci = fechaa[2] + "/" + fechaa[1] + "/" + fechaa[0];
 		return Fec_naci;
 	}
-	
+
 	public void insertarAlumno() {
 		String fecha = miVista.getFecha();
 		String nombre = miVista.getNombre();
@@ -60,6 +69,5 @@ public class Controlador {
 		System.out.println(fecha);
 		miModelo.insertarCliente(nombre, apellidos, tinte, mechas, fecha);
 	}
-	
-	
+
 }
